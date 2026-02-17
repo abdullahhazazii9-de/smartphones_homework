60c916560e592278f979d6b49104ca1b0f211e91
abdullahhazazii9-de <abdullah.hazazii9@gmail.com>
2026-02-17 19:36:42 +0300

    Create main.dart

---
+++ homework1/main.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: Scaffold(
+        body: Center(
+          child: Text(
+            'عبدالله عبدالعزيز هزازي\nرقمي التدريبي: 443229375',
+            textAlign: TextAlign.center,
+            style: TextStyle(
+              fontSize: 24,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
